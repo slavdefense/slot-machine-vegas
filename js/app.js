30100db5966187519e12c30a4018525b0d2ba717
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-10-15 19:43:01 -0700

    create basic style, write pseudo code for the app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('j')
+// Create  3 random numbers from 0 to 10 
+// Attach click handler to a play button so that when the button clicks, the numbers are created.
+// allow 3 combination of bet increasement.
+// allow all credit to be bet.
+//set a time interval of 1 second for the first number, 2 second for the second number and 3 seconds for the third number
+//attact svg images to each number 
+//display the svg images instead of the corresponding numbers
+// write conditional statements for any winning combinations
+// if winning combinations are satisfied, increase the credits.
+// if winning combinations are not satisfied, reduce the credits.
+//display a pay table at the click of a button
+// have an option to mute/turn on sound at a click of a button
+
