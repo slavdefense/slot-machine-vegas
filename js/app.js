fdbcd1d974f944fd0e627a285021ba83d9109348
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-10-16 01:32:38 -0700

    Create and define render function that takes the random number and matches it with a symbol on a reference array with objects that contains key value pairs of numbers and symbols

--- js/app.js
+++ js/app.js
@@ -19,7 +19,16 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+let credits, coins_played, winner_paid
+let results =[ ]
 
+const ref =[{9:'bar'},{8:"7"},{7:"7"},{6:
+"bell"},{5:"bell"},{4:'bell'},{3:
+"grape"},{2:"grape"},{1:"grape"},{0:"grape"}]
+
+
+
+const ourNum =[null,null,null]
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -27,13 +36,16 @@ const pay_btn = document.querySelector("#pay_btn")
 const bet_btn = document.querySelector("#bet_btn")
 const play_btn = document.querySelector("#play_btn")
 const withdrawBtn =document.querySelector("#withdraw_btn")
-
+const first_slot=document.querySelector("#slot-1")
+const second_slot=document.querySelector("#slot-2")
+const third_slot=document.querySelector("#slot-3")
 
 /*----------------------------- Event Listeners -----------------------------*/
 pay_btn.addEventListener("click",payChart)
 bet_btn.addEventListener("click",bet)
 play_btn.addEventListener("click",playGame)
 withdrawBtn.addEventListener("click",reset)
+
 /*-------------------------------- Functions --------------------------------*/
 init()
 
@@ -45,21 +57,50 @@ console.log('payChart')
 }
 
 function playGame(){
-console.log('played')
-let numberFirst=Math.floor(Math.random()*10)+1
-let numberSecond= Math.floor(Math.random()*10)+1
-let numberThird= Math.floor(Math.random()*10)+1
 
+let numberFirst = Math.floor(Math.random()*10)
+let numberSecond = Math.floor(Math.random()*10)
+let numberThird = Math.floor(Math.random()*10)
 console.log(numberFirst)
-console.log(numberSecond)
-console.log(numberThird)
+// ourNum[0]=numberFirst
+// ourNum[1]=numberSecond
+// ourNum[2]=numberThird
+// console.log (ourNum)
+render(ref,numberFirst,numberSecond,numberThird)
+// first_slot.innerText=numberFirst
+// second_slot.innerText=numberSecond
+// third_slot.innerText=numberThird
 
 }
+
+
+function render(ref,numberFirst,numberSecond,numberThird){
+  
+  for(item of ref){
+    for(let [key,value] of Object.entries(item)){
+      // console.log(`key:${key} value:${value}`)
+      if(parseInt(key)===numberFirst){
+        console.log(value)
+        
+      }
+      if(parseInt(key)===numberSecond){
+        console.log(value)
+      }
+      if(parseInt(key)===numberThird){
+        console.log(value)
+      }
+    }
+  }
+   }
+ 
+ 
 function bet(){
 console.log('betted')
 }
+
 function render(){
-  
+
+
 }
 function reset(){
   console.log("withdraw")
