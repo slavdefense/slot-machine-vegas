df8c78a0548c820f12f0ed03061aa835f91c45ed
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-10-15 19:59:43 -0700

    Create more psedo code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
 // Create  3 random numbers from 0 to 10 
-// Attach click handler to a play button so that when the button clicks, the numbers are created.
-// allow 3 combination of bet increasement.
-// allow all credit to be bet.
-//set a time interval of 1 second for the first number, 2 second for the second number and 3 seconds for the third number
-//attact svg images to each number 
-//display the svg images instead of the corresponding numbers
-// write conditional statements for any winning combinations
-// if winning combinations are satisfied, increase the credits.
-// if winning combinations are not satisfied, reduce the credits.
-//display a pay table at the click of a button
-// have an option to mute/turn on sound at a click of a button
+// Define our click handler to a play button, messages, bet amount,remaining credit, pay chart etc.
+// Allow players add credit card info and display the amount added. 
+// Create 3 combination of bet increasement.
+// Create all credit to be bet.
+//Define a time interval of 1 second for the first number, 2 second for the second number and 3 seconds for the third number
+//Attact svg images to each number 
+//Display  svg images instead of the corresponding numbers
+// Define conditional statements for any winning combinations
+// Pay credits if winning combinations are satisfied.
+// Reduce credits if winning combinations are not satisfied.
+//Create and display a pay table at the click of a button
+// Create options to mute/turn on sound at a click of a button
+// Create cash out option which resets all the values.
 
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
